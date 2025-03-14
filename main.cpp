9c52066426da7a259ea96ef51d34965aa1cbf366
nekotyan2d <dmitryrychazhkov2019@gmail.com>
2025-03-15 02:20:30 +0300

    Revert "feat: добавил иконку приложения"
    This reverts commit 2bb536f120c20a6057089750dd36614a48b45425.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,10 @@
 #include "FileManager.h"
 #include <QtWidgets/QApplication>
-#include <QIcon>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     FileManager w;
-
-	w.setWindowIcon(QIcon(":/FileManager/Resources/icon.png"));
     w.show();
     return a.exec();
 }
